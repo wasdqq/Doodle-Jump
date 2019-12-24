bec1e12d433e677fc1797c1eafe94271a15b0f74
wasdqq <59120360+wasdqq@users.noreply.github.com>
2019-12-24 22:15:22 +0200

    add big bounce platform generation option

---
+++ DoodleJump/Assets/BigBounce.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BigBounce : MonoBehaviour
+{
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    private void OnCollisionEnter2D(Collision2D collision2D)
+    {
+        if (collision2D.gameObject.GetComponent<Rigidbody2D>().velocity.y <= 0)
+        {
+            collision2D.gameObject.GetComponent<Rigidbody2D>().AddForce(Vector3.up * 1000f);
+        }
+    }
+}
--- DoodleJump/Assets/Destroy.cs
+++ DoodleJump/Assets/Destroy.cs
@@ -6,7 +6,8 @@ public class Destroy : MonoBehaviour
 {
 
     public GameObject player;
-    public GameObject platforPrefab;
+    public GameObject platformPrefab;
+    public GameObject springPrefab;
     private GameObject myPlat;
     
     
@@ -24,9 +25,23 @@ public class Destroy : MonoBehaviour
 
     private void OnTriggerEnter2D(Collider2D collider2D)
     {
-        myPlat = Instantiate(platforPrefab, 
-            new Vector2(Random.Range(-5.5f, 5.5f), player.transform.position.y + (14 + Random.Range(0.5f, 1f))), 
-            Quaternion.identity);
+
+        if (Random.Range(1, 6) > 1)
+        {
+
+            myPlat = Instantiate(platformPrefab,
+                new Vector2(Random.Range(-5.5f, 5.5f), player.transform.position.y + (14 + Random.Range(0.5f, 1f))),
+                Quaternion.identity);
+            //Destroy(collider2D.gameObject);
+        }
+        else
+        {
+            myPlat = Instantiate(springPrefab,
+                new Vector2(Random.Range(-5.5f, 5.5f), player.transform.position.y + (14 + Random.Range(0.5f, 1f))),
+                Quaternion.identity);
+            //Destroy(collider2D.gameObject);
+        }
+        
         Destroy(collider2D.gameObject);
     }
 }
