d27a2a73c842d31f8831db83d7fbc8324757fcec
wasdqq <59120360+wasdqq@users.noreply.github.com>
2019-12-24 19:36:41 +0200

    add platform generation script

---
+++ DoodleJump/Assets/Destroy.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Destroy : MonoBehaviour
+{
+
+    public GameObject player;
+    public GameObject platforPrefab;
+    private GameObject myPlat;
+    
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    private void OnTriggerEnter2D(Collider2D collider2D)
+    {
+        myPlat = Instantiate(platforPrefab, 
+            new Vector2(Random.Range(-5.5f, 5.5f), player.transform.position.y + (14 + Random.Range(0.5f, 1f))), 
+            Quaternion.identity);
+        Destroy(collider2D.gameObject);
+    }
+}
